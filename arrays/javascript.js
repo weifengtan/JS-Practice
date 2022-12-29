8344f4c1c1c8cc0e19637c146a5d88d9e01f3507
Alan Tan <alan@Alans-MacBook-Pro-2.local>
2022-12-29 09:27:25 -0800

    used splice, slice, shifting, unshifting array.isarry, and pop push and also to string and join

--- arrays/javascript.js
+++ arrays/javascript.js
@@ -22,3 +22,41 @@ console.log(ppl);
 console.log(Array.isArray(ppl));
 
 console.log(ppl.toString());
+
+console.log(ppl.join("-")); 
+
+console.log(ppl.pop()); 
+
+console.log(ppl.push("Martine"));
+
+console.log(ppl);
+
+console.log(ppl.shift());
+
+console.log(ppl);
+
+console.log(ppl.unshift("Joe")); 
+
+console.log(ppl); 
+
+const notppl = ["spiderman", "batman", "bat", "peeta"]; 
+
+const pplAndnotppl = ppl.concat(notppl); 
+
+console.log(pplAndnotppl); 
+
+const animals = ["pig", "bear", "fish", "dog", "cat"]; 
+
+const everything = animals.concat(ppl, notppl); 
+
+console.log(everything); 
+
+const fruits = ['Banna', 'apple', 'pear', 'mango', 'potato']; 
+
+fruits.splice(0,0);
+
+console.log(fruits); 
+
+const fruits2 = fruits.slice(2); 
+
+console.log(fruits2); 
